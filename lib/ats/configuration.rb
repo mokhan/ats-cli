0878dc92feebe759244cbed5f93c75012b8d84ac
mo <mo.khan@gmail.com>
2018-03-18 14:39:46 -0600

    record which config files are loaded.

--- lib/ats/configuration.rb
+++ lib/ats/configuration.rb
@@ -4,7 +4,7 @@ module ATS
 
     def initialize
       @logger = Logger.new(STDOUT)
-      @logger.level = Logger::ERROR
+      @logger.level = Logger::DEBUG
       @config_files = [
         File.join(Dir.home, ".atsrc"),
         File.expand_path('.atsrc'),
@@ -20,6 +20,7 @@ module ATS
 
     def load_configuration(files = config_files)
       files.inject({}) do |memo, file|
+        logger.debug("Loading... #{file}")
         memo.merge!(YAML.load(IO.read(file))) if File.exist?(file)
         memo
       end
